686cb7cdeed311db047df44be2b8c58b44c12aa9
akhilj95 <akhilmjohn@gmail.com>
2025-07-13 15:18:56 +0000

    added media and frame models

--- backend/missions/admin.py
+++ backend/missions/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
-from .models import Rover, Sensor, Calibration, Mission, SensorDeployment
+from .models import RoverHardware, Sensor, Calibration, Mission, SensorDeployment, NavSample, LogFile
 
 
-admin.site.register(Rover)
+admin.site.register(RoverHardware)
 admin.site.register(Sensor)
 admin.site.register(Calibration)
 admin.site.register(Mission)
 admin.site.register(SensorDeployment)
+admin.site.register(NavSample)
+admin.site.register(LogFile)
